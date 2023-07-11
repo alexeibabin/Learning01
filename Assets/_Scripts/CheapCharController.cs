024e5bcfd8873511415f22612e95814d274f4c72
Supreme <Supreme.main@gmail.com>
2023-07-11 18:15:16 +0300

    Added character and adjusted basic blend tree for movement

---
+++ Assets/_Scripts/CheapCharController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheapCharController : MonoBehaviour
+{
+    [SerializeField] Animator characterAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
