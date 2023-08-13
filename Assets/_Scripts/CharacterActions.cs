7074d0abfa179b362c9c24bc353cf100d623a4ea
Supreme <Supreme.main@gmail.com>
2023-08-13 23:18:20 +0300

    Added some WIP char controller logic

---
+++ Assets/_Scripts/CharacterActions.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class CharacterActions : PlayerActionSet
+{
+    public PlayerAction MoveLeft;
+    public PlayerAction MoveRight;
+    public PlayerAction MoveForward;
+    public PlayerAction MoveBackward;
+
+    public PlayerTwoAxisAction Move;
+
+    public CharacterActions()
+    {
+        MoveLeft = CreatePlayerAction("MoveLeft");
+        MoveRight = CreatePlayerAction("MoveRight");
+        MoveForward = CreatePlayerAction("MoveForward");
+        MoveBackward = CreatePlayerAction("MoveBackward");
+
+        Move = CreateTwoAxisPlayerAction(MoveLeft, MoveRight, MoveBackward, MoveForward);
+    }
+
+    public static CharacterActions GetDefaultPlayerActions()
+    {
+        CharacterActions ca = new CharacterActions();
+
+        ca.MoveLeft.AddDefaultBinding(Key.A, Key.LeftArrow);
+        ca.MoveRight.AddDefaultBinding(Key.D, Key.RightArrow);
+        ca.MoveForward.AddDefaultBinding(Key.W, Key.UpArrow);
+        ca.MoveBackward.AddDefaultBinding(Key.S, Key.DownArrow);
+
+        return ca;
+    }
+}
--- Assets/_Scripts/CheapCharController.cs
+++ Assets/_Scripts/CheapCharController.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using InControl;
+using Unity.VisualScripting;
 
 public class CheapCharController : MonoBehaviour
 {
     [SerializeField] Animator characterAnimator;
 
+    CharacterActions ca;
+
+    private void Start() {
+        InputManager.OnDeviceAttached += OnDeviceAttached;
+
+        ca = CharacterActions.GetDefaultPlayerActions();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if(ca.Move.HasChanged)
+        {
+            MoveCharacter(ca.Move.Value);
+            Debug.LogFormat("Move state changed: {0} ", ca.Move.Value);
+        }
+
+        if(ca.MoveLeft.HasChanged)
+        {
+            Debug.LogFormat("Move left(!) state changed: {0}", ca.Move.Value);
+        }
+    }
+
+    private void OnDeviceAttached(InputDevice device)
+    {
+        Debug.LogFormat("Current device : {0}", device.ToString());
+    }
+
+    private void MoveCharacter(Vector2 axis) {
+        characterAnimator.SetFloat("LeftOrRight", axis.x/2f);
+        characterAnimator.SetFloat("ForwardOrBackward", axis.y/2f);
     }
 }
