4a8f286027f62646dd0bd6fcf17db24f0f2bc051
Alexei Babin <>
2025-01-15 00:38:03 +0200

    Updated version, removed shitty player controller

--- Assets/_Scripts/CheapCharController.cs
+++ Assets/_Scripts/CheapCharController.cs
@@ -1,47 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using InControl;
-using Unity.VisualScripting;
 
 public class CheapCharController : MonoBehaviour
 {
     [SerializeField] Animator characterAnimator;
 
-    CharacterActions ca;
-
-    void Awake() {
-        InputManager.OnDeviceAttached += OnDeviceAttached;
-        ca = CharacterActions.GetDefaultPlayerActions();
-        ca.Enabled = true;
-
-        Debug.LogFormat("Currently active device: {0}", InputManager.ActiveDevice);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(ca.MoveForward.WasReleased) 
-        {
-            Debug.LogFormat("Went forward: {0} ", ca.Move.Value);
-        }
-
-        if(ca.Move.HasChanged)
-        {
-            MoveCharacter(ca.Move.Value);
-            Debug.LogFormat("Move state changed: {0} ", ca.Move.Value);
-        }
-
-        if(ca.MoveLeft.HasChanged)
-        {
-            Debug.LogFormat("Move left(!) state changed: {0}", ca.Move.Value);
-        }
-    }
-
-    private void OnDeviceAttached(InputDevice device)
-    {
-        Debug.LogFormat("Current device : {0}", device.GetType());
-    }
+void Update()
+{
+    // Move the player
+    MoveCharacter(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+}
 
     private void MoveCharacter(Vector2 axis) {
         characterAnimator.SetFloat("LeftOrRight", axis.x/2f);
